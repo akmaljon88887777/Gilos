75e305a83b65355fd0829563e28cef39e92d7ae8
Timur <example@gmail.com>
2024-05-18 19:48:41 +0500

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Routers from "../Router/index";
+function App() {
+  return (
+    <div className="App bg-pink sm:bg-black lg:bg-blue w-full h-screen">
+      <Routers />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#FF0000",
+        blue: "#0000FF",
+        white: "#fff",
+        black: "#000",
+        green: "#008000",
+        yellow: "#FFFF00",
+        pink: "#FFC0CB",
+      },
+      screens: {
+        xs: "375px",
+        sm: "425px",
+        ss: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
