0a158721c18feb5b43a2eaa429f2d7601e3a8f14
Timur <example@gmail.com>
2024-05-18 19:51:22 +0500

    salom

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Routers from "../Router/index";
 function App() {
   return (
-    <div className="App bg-pink sm:bg-black lg:bg-blue w-full h-screen">
+    <div className="App bg-pink sm:bg-black lg:bg-blue">
       <Routers />
     </div>
   );
